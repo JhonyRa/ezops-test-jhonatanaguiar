994c5ad8c6f569bd5b93a2295a9ac709f2f3f7c0
JhonyRa <43555988+JhonyRa@users.noreply.github.com>
2021-05-17 18:39:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ mongoose.connect(dbUrl, {useNewUrlParser: true,  useUnifiedTopology: true}).then
   console.log(err);
 });
 
-var server = http.listen(3002, () => {
+var server = http.listen(3000, () => {
   console.log('server is running on port', server.address().port);
 });
