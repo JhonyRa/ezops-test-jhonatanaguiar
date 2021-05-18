ee9ac121b4bfbfe5d0702a99829f7421c289ba5d
JhonyRa <43555988+JhonyRa@users.noreply.github.com>
2021-05-18 15:16:05 -0300

    uri mongodb

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ var Message = mongoose.model('Message',{
   message : String
 })
 
-var dbUrl = 'mongodb+srv://atlasAdmin:Abacaxi123@ezops.aovgr.mongodb.net/ezops-database?retryWrites=true&w=majority'
+var dbUrl = 'mongodb+srv://ezops-shard-00-01.aovgr.mongodb.net'
+
 
 app.get('/messages', (req, res) => {
   Message.find({},(err, messages)=> {
